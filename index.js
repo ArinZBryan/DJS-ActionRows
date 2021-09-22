e56aefa3281c61a16358f729a039e1ca106f754c
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2021-09-22 14:57:39 +0100

    cleared whitespace

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-
-
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
